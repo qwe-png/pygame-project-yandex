faed8d911b547ab7b720c22a58fe92b0f6da1dcc
Evgeniy <turskov07081@gmail.com>
2021-12-19 17:53:24 +0300

    Изменен класс board

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 
 class Board:
@@ -8,42 +9,59 @@ class Board:
         self.height = height
         self.board = [[0] * width for _ in range(height)]
         # значения по умолчанию
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
+        self.set_view()
 
     # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
+    def set_view(self, left=10, top=10, cell_size=30):
         self.left = left
         self.top = top
         self.cell_size = cell_size
 
     def render(self, screen):
+        w = pygame.Color(255, 255, 255)
+        cs = self.cell_size
         for y in range(self.height):
             for x in range(self.width):
-                pygame.draw.rect(screen, pygame.Color(255, 255, 255), (x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, self.cell_size), 1)
-                for event in pygame.event.get():
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        print('(' + str((int(event.pos[0]) - int(self.left)) // self.cell_size) + ', ' + str((int(event.pos[1]) - int(self.top)) // self.cell_size) + ')' if not ((int(event.pos[0]) - int(self.left)) // self.cell_size) < 0 and not ((int(event.pos[0]) - int(self.left)) // self.cell_size) >= self.width and not ((int(event.pos[1]) - int(self.top)) // self.cell_size) < 0 and not ((int(event.pos[1]) - int(self.top)) // self.cell_size) >= self.height else None)
+                pygame.draw.rect(screen, w, (x * cs + self.left, y * cs + self.top, cs, cs), 1)
 
-def main():
+    def get_cell(self, mouse_pos):
+        x = math.ceil((mouse_pos[0] - self.left) / self.cell_size) - 1
+        y = math.ceil((mouse_pos[1] - self.top) / self.cell_size) - 1
+        try:
+            if x >= 0 and y >= 0:
+                check = self.board[y][x]
+                return (x, y)
+            else:
+                return None
+        except IndexError:
+            return None
+
+    def on_click(self, cell_coords):
+        pass
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+
+if __name__ == '__main__':
     pygame.init()
-    size = 600, 600
+    pygame.display.set_caption('Игра')
+
+    size = width, height = 800, 800
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Инициализация игры')
-    board = Board(5, 7)
-    board.set_view(100, 100, 30)
+    screen.fill(pygame.Color("blue"))
+    x, y = 800, 800
+    board = Board(x, y)
+    board.set_view(300, 300, 50)
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                board.get_click(event.pos)
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
-    pygame.quit()
-
-
-if __name__ == '__main__':
-    main()
-    #,
