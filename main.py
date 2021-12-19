361d3965787609704df9c5bd53019341ef2ea315
Evgeniy <turskov07081@gmail.com>
2021-12-19 17:46:08 +0300

    Удален класс path

--- main.py
+++ main.py
@@ -25,67 +25,7 @@ class Board:
                                  (i * self.cell_size + self.left, j * self.cell_size + self.top, self.cell_size,
                                   self.cell_size), 2)
 
-class Path(Board):
-    import pygame
-    import random
 
-    class LOL(AssertionError):
-        pass
-
-    if __name__ == '__main__':
-        try:
-
-            # инициализация Pygame:
-            pygame.init()
-            pygame.display.set_caption('Дичь')
-            # размеры окна:
-            size = width, height = 300, 200
-            # screen — холст, на котором нужно рисовать:
-            screen = pygame.display.set_mode(size)
-            screen.fill(pygame.Color('white'))
-            s = 0
-            w = 0
-            g = 0
-            u = 0
-            r = 0
-            m = 0
-            provershik = 0
-            for i in range(300):
-                w = 30
-                provershik += 1
-                if u == 1:
-                    s -= 15
-                u = 0
-                if provershik % 11 == 0:
-                    provershik = 0
-                    w = 15
-                    g += 17
-                    u = 1
-                    s = 0
-                    r += 1
-                    m += 1
-                    if r % 2 == 0:
-                        s -= 16
-                screen.fill(pygame.Color('red'), (s, g, w, 15))
-                s += 32
-                if u % 2 == 1:
-                    screen.fill(pygame.Color('red'), (0, g, w, 15))
-            # формирование кадра:
-            # команды рисования на холсте
-            # ...
-            # ...
-            # смена (отрисовка) кадра:
-            pygame.display.flip()
-            # ожидание закрытия окна:
-
-            while pygame.event.wait().type != pygame.QUIT:
-                pass
-            # завершение работы:
-            pygame.quit()
-        except ValueError:
-            print('Неправильный формат ввода')
-        except LOL:
-            print('Число n не кратно числу a')
 size = width, height = 800, 400
 screen = pygame.display.set_mode(size)
 board = Board(5, 7)
